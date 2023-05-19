611144ca75e1d50b7324d4266b2f70801c1395e9
NCInside <nchristian02@student.ciputra.ac.id>
2023-05-19 21:34:47 +0700

    add model and resource structure

---
+++ MovieHub2/Model/Movie.swift
@@ -0,0 +1,25 @@
+//
+//  Movie.swift
+//  MovieHub2
+//
+//  Created by MacBook Pro on 19/05/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Movie: Hashable, Codable, Identifiable {
+    var id: Int
+    var title: String
+    var score: Double
+    var genres: [String]
+    var rating: String
+    var synopsis: String
+    var cast: [String: String]
+    var duration: Int
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+}
--- MovieHub2/Views/Movie/MovieCard.swift
+++ MovieHub2/Views/Movie/MovieCard.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct MovieCard: View {
+    
+    var movie: Movie
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            movie.image
+                .resizable()
+            HStack {
+                ForEach (0..<Int(movie.score), id: \.self) {_ in
+                    Image(systemName: "star.fill")
+                }
+                ForEach (0..<Int(5-movie.score), id: \.self) {_ in
+                    Image(systemName: "star.fill")
+                }
+            }
+            Text(movie.title)
+            HStack {
+                Text(movie.genres[0])
+                Text("|")
+                Text("\(movie.duration / 60)h \(movie.duration % 60)m")
+                Text("|")
+                Text(movie.rating)
+            }
+        }
     }
 }
 
 struct MovieCard_Previews: PreviewProvider {
+    static var movies = ModelData().movies
+    
     static var previews: some View {
-        MovieCard()
+        MovieCard(movie: movies[0])
     }
 }
